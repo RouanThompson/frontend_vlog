319babf4242dd60e8541dfde95123b4cc80df91c
Rouan Thompson <rouan.thompson@gmail.com>
2020-12-01 23:20:35 -0500

    added labels to form control &  changed form to fieldset

--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -55,15 +55,29 @@ class SignUp extends React.Component{
 
         return(
             <>
-                <form onSubmit={this.handleSubmit} >
-                    <h1>Sign Up!</h1>
-                    <input name="firstName" placeholder="First Name" value={this.state.firstName} onChange={this.handleChange}/>
-                    <input name="lastName" placeholder="Last Name" value={this.state.lastName} onChange={this.handleChange} />
-                    <input name="username" placeholder="User Name" value={this.state.username} onChange={this.handleChange} />
+                <fieldset onSubmit={this.handleSubmit} >
+                    {/* fieldset tag groups field controls together and has line around them to show they are grouped*/}
+                    {/* form controls are tags used to gather user input */}
+                    {/* fieldset lines can be changed with CSS */}
+                    <legend>Sign Up!</legend>
+                    {/* legend tag is used as caption of the purpose of the group form controls */}
+                    <label for="firstname">First Name: <input id="firstname" type="text" name="firstName" placeholder="First Name" value={this.state.firstName} onChange={this.handleChange} required/> </label>
+                    {/* label tags are being used for the vision impaired user */}
+                    {/* the for attribute use the id attribute value of the form control it is labeling*/}
+                    <br />
+                    {/* elements with no words between open and closing tags are empty elements*/}
+                    <label for="lastname">Last Name: <input id="lastname" type="text" name="lastName" placeholder="Last Name" value={this.state.lastName} onChange={this.handleChange} required/> </label>
+                    <br />
+                    <label for="username">Username: <input id="username" type="text" name="username" placeholder="User Name" value={this.state.username} onChange={this.handleChange} required/> </label>
                     {/* <input name="email" placeholder="email" /> */}
-                    <input name="password" placeholder="Password" value={this.state.password} onChange={this.handleChange} />
+                    <br />
+                    <label for="password">Password: <input id="password" type="password" name="password" placeholder="Password" value={this.state.password} onChange={this.handleChange} required/></label>
+                    {/* type attribute set to password hides the input in the password field when the user types */}
+                    {/* ...this does not make the password secure when the form is submit */}
+                    <br />
                     <button> Create Account </button>
-                </form>
+                    {/* button does not need type attribut to be set to submit */}
+                </fieldset>
                 <div>
                     Already have an account? Login
                 </div>
